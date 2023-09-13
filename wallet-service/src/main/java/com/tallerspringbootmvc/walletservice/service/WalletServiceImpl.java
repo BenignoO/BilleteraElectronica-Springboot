b3fe3482d0d8f8d879caf655aca465563ae4b11a
Benigno Orellana <jbenigno.2993@gmail.com>
2023-09-13 16:55:47 -0600

    Modificando Client

--- wallet-service/src/main/java/com/tallerspringbootmvc/walletservice/service/WalletServiceImpl.java
+++ wallet-service/src/main/java/com/tallerspringbootmvc/walletservice/service/WalletServiceImpl.java
@@ -1,10 +1,13 @@
 package com.tallerspringbootmvc.walletservice.service;
 
 import com.tallerspringbootmvc.walletservice.dto.WalletRequestDTO;
+import com.tallerspringbootmvc.walletservice.model.WalletEntity;
 import com.tallerspringbootmvc.walletservice.model.mapper.WalletMapper;
 import com.tallerspringbootmvc.walletservice.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,14 @@ public class WalletServiceImpl implements WalletService{
     @Override
     public void createWallet(WalletRequestDTO requestDTO) {
 
+        if (requestDTO.clientDocumentNumber().trim().isEmpty())
+            throw new RuntimeException("El número de documento del cliente debe ser válido.");
+
+        WalletEntity wallet = WalletEntity.builder()
+                .clientDocumentNumber(requestDTO.clientDocumentNumber())
+                .balance(BigDecimal.valueOf(0))
+                .currency("USD")
+                .build();
+        walletRepository.save(wallet);
     }
 }
